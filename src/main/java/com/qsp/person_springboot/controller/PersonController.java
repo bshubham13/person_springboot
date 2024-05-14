4101fbf0d5f983c9a87e2d01b259a4f80de7edf2
bshubham13 <bshbham1122@gmail.com>
2024-05-14 10:15:23 +0530

    Update PersonController.java

--- src/main/java/com/qsp/person_springboot/controller/PersonController.java
+++ src/main/java/com/qsp/person_springboot/controller/PersonController.java
@@ -3,10 +3,12 @@ package com.qsp.person_springboot.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.hibernate.grammars.hql.HqlParser.IsEmptyPredicateContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qsp.person_springboot.dto.Person;
@@ -34,11 +36,11 @@ public class PersonController {
 	public String savePersonAll(@RequestBody List<Person>list) {
 		
 		List<Person> person2= repo.saveAll(list);
-		if(person2!=null) {
-			return "save successfull";
+		if(person2.isEmpty()) {
+			return "Failed to save";
 			}
 			else {
-				return "failed to save";
+				return "save successfull";
 			}
 		} 
 	
@@ -54,6 +56,13 @@ public class PersonController {
 		}
 	}
 	
+	
+	//Alternate way to fetch data
+	@GetMapping("/find")
+	public Person findPerson(@RequestParam int id) {
+		return repo.findById(id).get();
+	}
+	 
 		
 	}
 	
